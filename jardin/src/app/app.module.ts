8d783b344f0aedb7b1c8cf0884e00c77be0f74b1
JuanHerreraSalcedo <juank.hs5500@gmail.com>
2023-03-23 01:21:15 -0500

    se hace vista login, se ajusta home y se crea registro

--- jardin/src/app/app.module.ts
+++ jardin/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { InstitucionalComponent } from './components/pages/institucional/institucional.component';
 import { BannerHomeComponent } from './components/modules/banner-home/banner-home.component';
 import { BannerInstitucionalComponent } from './components/modules/banner-institucional/banner-institucional.component';
+import { IniciarSesionComponent } from './login/iniciar-sesion/iniciar-sesion.component';
+import { RegistroComponent } from './login/registro/registro.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +20,9 @@ import { BannerInstitucionalComponent } from './components/modules/banner-instit
     HomeComponent,
     InstitucionalComponent,
     BannerHomeComponent,
-    BannerInstitucionalComponent
+    BannerInstitucionalComponent,
+    IniciarSesionComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
