7e77d4012f01fc8ef0519553f024f9b2293356d1
JuanHerreraSalcedo <juank.hs5500@gmail.com>
2023-04-07 16:21:33 -0500

    se integraron varias vistas

--- jardin/src/app/app-routing.module.ts
+++ jardin/src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { IniciarSesionComponent } from './login/iniciar-sesion/iniciar-sesion.component';
 import { RegistroComponent } from './login/registro/registro.component';
+import { InstitucionalComponent } from './components/pages/institucional/institucional.component';
+import { CursosComponent } from './components/pages/cursos/cursos.component';
+
+// modal
+import { MisionComponent } from './modal/mision/mision.component';
 
 const routes: Routes = [
   {path: '', pathMatch:'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'iniciar-sesion', component: IniciarSesionComponent},
   {path: 'registro', component: RegistroComponent},
+  {path: 'institucional', component: InstitucionalComponent},
+  {path: 'cursos', component: CursosComponent},
+
+
+  {path: 'mision', component: MisionComponent}
 ];
 
 @NgModule({
